378dd5c8f86342bcee54d632e3fb06cb14aab038
Jonathan Fields <jonathanfields@gmail.com>
2021-05-26 22:55:08 -0400

    Update SolutionB.java

--- Problem 3/SolutionB.java
+++ Problem 3/SolutionB.java
@@ -1,6 +1,6 @@
 /*************************************************************
- Purpose/Description: A java class which implements a sqaure /
- multiply algorithm to efficently calculate exponents
+ Purpose/Description: A java class to efficiently calculate xponentiation 
+ using a recurssive implementation of the expo by square algorithm
  Author’s Panther ID: 6231951
  Certification:I hereby certify that this work is my own and
  none of it is the work of any other person.
@@ -8,6 +8,31 @@
 
 class SolutionB {
 	static long N = 1000000007L; // prime modulo value
+
+	static long exponentiation(long base, long exp)
+	{
+		if (exp == 0)
+		{
+			return 1;
+		}
+
+		if (exp == 1)
+		{
+			return base % N;
+		}
+		long t = exponentiation(base, exp / 2);
+		t = (t * t) % N;
+
+		// if exponent is even value
+		if (exp % 2 == 0){
+			return t;
+		}
+		// if exponent is odd value
+		else{
+			return ((base % N) * t) % N;
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		long base = 5;
@@ -16,21 +41,4 @@ class SolutionB {
 		long modulo = exponentiation(base, exp);
 		System.out.println(modulo);
 	}
-
-	static long exponentiation(long base, long exp)
-	{
-		long t = 1L;
-		while (exp > 0) {
-
-			// for cases where exponent
-			// is not an even value
-			if (exp % 2 != 0)
-				t = (t * base) % N;
-
-			base = (base * base) % N;
-			exp /= 2;
-		}
-		return t % N;
-	}
 }
-		
